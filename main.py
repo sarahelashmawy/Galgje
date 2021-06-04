ad4d2eff76d2d1fc53f71ed167cea75f9d3a060d
Sarah Elashmawy <>
2021-06-04 08:44:38 +0000

    string van woorden

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 
 print("Welkom bij galgje! Het gaat erom dat je door de computer gekozen woord gaat raden. Maar pas op, je aantal levens zijn beperkt. Raad het woord voordat je levens op zijn!")
 
+print("Ben je klaar om te starten?")
+
+
+woorden = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
