0149d01e7cdb190ccd5d9cee7c95be2c9d552d4a
Sarah Elashmawy <>
2021-06-04 08:37:40 +0000

    begin gemaakt

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 
-print("Welkom bij galgje!")
+print("Welkom bij galgje! Het gaat erom dat je door de computer gekozen woord gaat raden. Maar pas op, je aantal levens zijn beperkt. Raad het woord voordat je levens op zijn!")
+
