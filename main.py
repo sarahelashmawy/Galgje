7ed08ea3896aa21a9c41331cd5fe9e420a7cf316
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2021-06-10 18:06:39 +0000

    1 letter kan alleen ingevoerd worden

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 woorden = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
 
@@ -8,8 +8,9 @@ woorden = ["informatica","informatiekunde","spelletje","aardigheidje","scholier"
 #woord = woorden[k]
 #print(woord)
 
-raad = 0
+raad = 1
 kanzen = 5
+raden = kanzen - raad
 gevonden = False
 correct = False
 amount = 0
@@ -21,20 +22,21 @@ veld = []
 
 print("Welkom bij galgje! Het gaat erom dat je door de computer gekozen woord gaat raden. Maar pas op, je aantal levens zijn beperkt. Raad het woord voordat je levens op zijn!")
 
-begin = input("Wil je beginnen?\n")
+begin = input("Wil je beginnen? JA of NEE?\n")
 while vast == True:
   veld.clear()
   gekozen.clear()
   woord = random.choice(woorden)
+  
   #print (woord)
   lijstwoord = list(woord)
-  if begin == "ja":
+  if begin == "JA":
     for x in range (len(lijstwoord)):
         veld.append("_")
     
-
   while raad <= kanzen and lijstwoord != veld:
     keuze = input("Noem een letter:\n")
+    
 
   #check for letter
     for z in range (len(lijstwoord)):
@@ -50,11 +52,24 @@ while vast == True:
             if amount == 0:
                 break            
         else:
-            gevonden = False        
-        
-    gekozen.append(keuze)
+            gevonden = False 
+
+    gekozen.append(keuze) 
+ 
+  # Alleen 1 letter invoeren
+    if len(keuze) > 1:
+      print("Je kan alleen 1 letter invoeren!")
+      print("Je hebt nog " + str(raden) + " keren om het woord te raden.")
+      print( )
+    if gevonden == False:
+      raad += 1
+      continue   
+
+  #Alleen letters, geen getallen  
+   if   
 
   #Letters in word   
+
     if gevonden == True:
         print(keuze + " komt voor in het woord\n")
         print("Je hebt de volgende letters gekozen:\n")
@@ -70,7 +85,7 @@ while vast == True:
     else:
         gevonden = False
 
-    if lijstwoord == veld :
+    if lijstwoord == veld:
       print("Je hebt alle letters geraden! Gefeliciteerd!")
       break
 
@@ -81,7 +96,7 @@ while vast == True:
     print("Jammer. Je hebt het woord niet geraden.") 
     raad = 0      
 
-  opnieuw = input("Wil je opnieuw spelen?\n")
+  opnieuw = input("Wil je opnieuw spelen?JA of NEE?\n")
 
   if opnieuw == "nee":             
     print("Dankje wel voor het spelen")
