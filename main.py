e68467a28be546fc90ca3f8b21e1988fc8e74bb1
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2021-06-11 08:55:03 +0000

    opknappen

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ veld = []
 
 
 print("Welkom bij galgje! Het gaat erom dat je door de computer gekozen woord gaat raden. Maar pas op, je aantal levens zijn beperkt. Raad het woord voordat je levens op zijn!")
+print( )
 
 begin = input("Wil je beginnen? JA of NEE?\n")
 while vast == True:
